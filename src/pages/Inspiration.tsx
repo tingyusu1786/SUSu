4c13ae69fe3bda0616af5c28cca5d63ebb83c354
tingyusu1786 <tingyusu1786@gmail.com>
2023-04-21 00:53:16 +0800

    update inspiration page checkbox style

--- src/pages/Inspiration.tsx
+++ src/pages/Inspiration.tsx
@@ -21,8 +21,8 @@ function Inspiration() {
   const allBrandsInfo = useAppSelector((state) => state.info.brands);
   const [selectedBrands, setSelectedBrands] = useState<string[]>([]);
   const [selectedRating, setSelectedRating] = useState<number>();
-  const [showBrands, setShowBrands] = useState(false);
-  const [showRatings, setShowRatings] = useState(false);
+  const [showBrands, setShowBrands] = useState(true);
+  const [showRatings, setShowRatings] = useState(true);
   const [randomItem, setRandomItem] = useState<RandomItem | null>();
   const [noItemMessage, setNoItemMessage] = useState<string>();
   const [isFinding, setIsFinding] = useState(false);
@@ -31,7 +31,7 @@ function Inspiration() {
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       const { latitude, longitude } = position.coords;
-      console.log('latitude', latitude, 'longitude', longitude);
+      // console.log('latitude', latitude, 'longitude', longitude);
       setCurrentLocation({ latitude, longitude });
     });
   }, []);
@@ -138,128 +138,124 @@ function Inspiration() {
 
   return (
     <main
-      className='min-h-screen bg-fixed'
+      className='flex min-h-[calc(100vh-64px)] flex-col items-center bg-fixed p-10'
       style={{
         backgroundImage:
           'linear-gradient(#BEEFCE 1px, transparent 1px), linear-gradient(to right, #BEEFCE 1px, #F6F6F9 1px)',
         backgroundSize: '20px 20px',
       }}
     >
-      <div className='text-center font-heal text-3xl'>what to drink today?</div>
-      <div className='flex flex-col items-center justify-center'>
-        <button
-          className='font-bold'
-          onClick={() => {
-            selectedBrands.length === 0 && setShowBrands((prev) => !prev);
-          }}
-        >
-          篩選店家
-        </button>
-        <div className={`flex gap-3 ${showBrands === false && 'hidden'}`}>
-          <button className='text-sm text-gray-400' onClick={() => setSelectedBrands([])}>
-            清除
-          </button>
-          {Object.keys(allBrandsInfo).map((key) => (
-            <label key={key}>
-              <input type='checkbox' name={key} checked={selectedBrands.includes(key)} onChange={handleBrandsChange} />
-              <span>{allBrandsInfo[key].name}</span>
-            </label>
-          ))}
-        </div>
-      </div>
-      <div className='flex flex-col items-center justify-center'>
-        <button
-          className='font-bold'
-          onClick={() => {
-            !selectedRating && setShowRatings((prev) => !prev);
-          }}
-        >
-          篩選評分
-        </button>
-        <div className={`flex gap-3 ${showRatings === false && 'hidden'}`}>
-          <button className='text-sm text-gray-400' onClick={() => setSelectedRating(undefined)}>
-            清除
-          </button>
-          {[1, 2, 3, 4].map((num) => (
-            <label key={num}>
-              <input
-                type='radio'
-                name='rating'
-                value={num}
-                checked={selectedRating === num}
-                onChange={handleRatingChange}
-              />
-              <span>
-                {Array(num)
-                  .fill(0)
-                  .map((n) => '⭐️')}
-                +
-              </span>
-            </label>
-          ))}
-        </div>
-      </div>
-      <button
-        className='rounded bg-lime-300 px-2 font-heal'
-        onClick={() => getRandomItem(selectedBrands, selectedRating)}
-      >
-        GO!
-      </button>
-      {isFinding && <div>choosing drinks for you~~~</div>}
-      {!isFinding && randomItem && (
+      <h1 className='mb-10 text-center text-7xl'>In the mood for something new?</h1>
+      <div className='flex max-w-3xl flex-col content-center'>
         <div className='flex flex-col items-center justify-center'>
-          <p>推薦你喝</p>
-          <Link to={`/catalogue/${randomItem.brandId}`}>
-            <p className='text-xl'>{randomItem.brand}</p>
-          </Link>
-          <span>的</span>
-          <Link to={`/catalogue/${randomItem.brandId}/${randomItem.itemId}`}>
-            <h1 className='text-3xl'>\ {randomItem.name} /</h1>
-          </Link>
-          {randomItem.averageRating && (
-            <p>
-              {randomItem.averageRating} / {randomItem.numRatings}
-            </p>
-          )}
-          {randomItem.price &&
-            Object.entries(randomItem.price).map((p) => (
-              <p key={p[0]}>
-                {p[0]}: ${p[1]}
-              </p>
+          {/*<div
+            className='self-start font-bold'
+            onClick={() => {
+              selectedBrands.length === 0 && setShowBrands((prev) => !prev);
+            }}
+          >
+            篩選店家
+          </div>*/}
+          <div className={`flex flex-wrap justify-start gap-3 ${showBrands === false && 'hidden'}`}>
+            {Object.keys(allBrandsInfo).map((key) => (
+              <label
+                key={key}
+                className={`button cursor-pointer rounded-full border-2 border-solid border-neutral-900 px-2 pt-1 transition-all duration-100 hover:bg-amber-400 ${
+                  selectedBrands.includes(key) ? 'bg-amber-400' : 'bg-transparent'
+                } ${selectedBrands.length !== 0 && !selectedBrands.includes(key) && 'opacity-40'}`}
+              >
+                <input
+                  type='checkbox'
+                  name={key}
+                  checked={selectedBrands.includes(key)}
+                  onChange={handleBrandsChange}
+                  className='hidden'
+                />
+                <span className={``}>{allBrandsInfo[key].name}</span>
+              </label>
             ))}
-          附近的店：
-          <iframe
-            width='640'
-            height='480'
-            style={{ border: 0, marginBottom: 5 }}
-            loading='lazy'
-            allowFullScreen
-            referrerPolicy='no-referrer-when-downgrade'
-            src={`https://www.google.com/maps/embed/v1/search?key=AIzaSyAyK3jgOTFT1B6-Vt85wxc_2aaGLUlU738
-                &q=${randomItem.brand}+nearby&language=zh-TW&center=${Number(currentLocation?.latitude)},${Number(
-              currentLocation?.longitude
-            )}&zoom=13`}
-          ></iframe>
-          {/*到最近（？）店的路線
-          <iframe
-            width='640'
-            height='480'
-            style={{ border: 0 }}
-            loading='lazy'
-            allowFullScreen
-            referrerPolicy='no-referrer-when-downgrade'
-            src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAyK3jgOTFT1B6-Vt85wxc_2aaGLUlU738
-                &destination=${randomItem.brand}&origin=${Number(currentLocation?.latitude)},${Number(
-              currentLocation?.longitude
-            )}&mode=walking`}
-          ></iframe>*/}
+            <span className='text-sm text-gray-400' onClick={() => setSelectedBrands([])}>
+              clear
+            </span>
+          </div>
         </div>
-      )}
-      {!isFinding && noItemMessage && <div>{noItemMessage}</div>}
-      {/*      <Button type='confirm' disabled={false} />
-      <Button type='confirm' disabled={true} />
-      <Button type='cancel' disabled={false} />
-      <Button type='edit' disabled={false} />*/}
+        <div className='flex flex-col items-center justify-center'>
+          {/*<div
+            className='font-bold'
+            onClick={() => {
+              !selectedRating && setShowRatings((prev) => !prev);
+            }}
+          >
+            篩選評分
+          </div>*/}
+          <div className={`flex gap-3 ${showRatings === false && 'hidden'}`}>
+            {[1, 2, 3, 4].map((num) => (
+              <label key={num}>
+                <input
+                  type='radio'
+                  name='rating'
+                  value={num}
+                  checked={selectedRating === num}
+                  onChange={handleRatingChange}
+                />
+                <span>
+                  {Array(num)
+                    .fill(0)
+                    .map((n) => '⭐️')}
+                  +
+                </span>
+              </label>
+            ))}
+            <span className='text-sm text-gray-400' onClick={() => setSelectedRating(undefined)}>
+              clear
+            </span>
+          </div>
+        </div>
+        <button
+          className='rounded bg-lime-300 px-2 font-heal'
+          onClick={() => getRandomItem(selectedBrands, selectedRating)}
+        >
+          GO!
+        </button>
+        {isFinding && <div>choosing drinks for you~~~</div>}
+        {!isFinding && randomItem && (
+          <div className='flex flex-col items-center justify-center'>
+            <p>推薦你喝</p>
+            <Link to={`/catalogue/${randomItem.brandId}`}>
+              <p className='text-xl'>{randomItem.brand}</p>
+            </Link>
+            <span>的</span>
+            <Link to={`/catalogue/${randomItem.brandId}/${randomItem.itemId}`}>
+              <h1 className='text-3xl'>\ {randomItem.name} /</h1>
+            </Link>
+            {randomItem.averageRating && (
+              <p>
+                {randomItem.averageRating} / {randomItem.numRatings}
+              </p>
+            )}
+            {randomItem.price &&
+              Object.entries(randomItem.price).map((p) => (
+                <p key={p[0]}>
+                  {p[0]}: ${p[1]}
+                </p>
+              ))}
+            附近的店：
+            <iframe
+              width='640'
+              height='480'
+              style={{ border: 0, marginBottom: 5 }}
+              loading='lazy'
+              allowFullScreen
+              referrerPolicy='no-referrer-when-downgrade'
+              src={`https://www.google.com/maps/embed/v1/search?key=AIzaSyAyK3jgOTFT1B6-Vt85wxc_2aaGLUlU738
+                &q=${randomItem.brand}+nearby&language=zh-TW&center=${Number(currentLocation?.latitude)},${Number(
+                currentLocation?.longitude
+              )}&zoom=13`}
+            ></iframe>
+          </div>
+        )}
+        {!isFinding && noItemMessage && <div>{noItemMessage}</div>}
+      </div>
     </main>
   );
 }
