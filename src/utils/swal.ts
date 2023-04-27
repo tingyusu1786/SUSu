7a2ad2c563a3869c0e17a510ad2eaf5a45e66bb4
tingyusu1786 <tingyusu1786@gmail.com>
2023-04-27 17:11:39 +0800

    edit comments

--- src/utils/swal.ts
+++ src/utils/swal.ts
@@ -51,8 +51,6 @@ const Error = Swal.mixin({
   background: '#f5f5f5',
 });
 
-// You won't be able to revert this!
-
 const swal = {
   warning(title: string, text: string, confirmButtonText: string) {
     const result = Warning.fire({ title, text, confirmButtonText });
@@ -72,115 +70,10 @@ const swal = {
   },
 };
 
-// Swal.fire({
-//   title: 'Do you really want to sign out?',
-//   showCancelButton: true,
-//   confirmButtonText: 'Yes',
-//   customClass: {
-//     cancelButton: 'order-1 right-gap',
-//     confirmButton: 'order-2',
-//   },
-//   confirmButtonColor: '#4ade80',
-// });
-
-// Toast.fire({
-//   icon: 'success',
-//   confirmButtonText: 'bye',
-//   title: 'signed out. see u next time 👋',
-// });
-
-// Toast.fire({
-//   icon: 'error',
-//   title: 'something went wrong...',
-// });
-
 // Toast.fire({
 //   icon: 'success',
 //   title: 'Signed up successful 🫰',
 //   confirmButtonText: 'ya',
-// });
-
-// MySwal.fire({
-//   title: `☹️ ${errorCode}`,
-//   icon: 'error',
-//   confirmButtonColor: '#b91c1c',
-// });
-
-// const result = await Swal.fire({
-//   title: 'Do you want to delete this comment?',
-//   text: 'this cannot be undone',
-//   showCancelButton: true,
-//   confirmButtonText: 'Yes',
-//   customClass: {
-//     cancelButton: 'order-1 right-gap',
-//     confirmButton: 'order-2',
-//   },
-//   confirmButtonColor: '#4ade80',
-// });
-
-// Swal.fire({
-//   title: 'Comment deleted!',
-//   icon: 'success',
-//   confirmButtonColor: '#4ade80',
-//   // showClass: { popup: '' },
-// });
-
-// const result = await Swal.fire({
-//   title: 'Delete this log?',
-//   text: "You won't be able to revert this",
-//   showCancelButton: true,
-//   confirmButtonText: 'Yes',
-//   customClass: {
-//     cancelButton: 'order-1 right-gap',
-//     confirmButton: 'order-2',
-//   },
-//   confirmButtonColor: '#4ade80',
-// });
-
-// Swal.fire({ title: 'Log deleted!', icon: 'success', confirmButtonColor: '#4ade80' });
-
-// Toast.fire({
-//   icon: 'success',
-//   title: `Welcome back ${userData?.name} 🫰`,
-//   confirmButtonText: 'hi!',
-// });
-
-// MySwal.fire({
-//   title: `☹️ ${errorCode}`,
-//   icon: 'error',
-//   confirmButtonColor: '#b91c1c',
-// });
-
-// MySwal.fire({
-//   title: `☹️ error: email used (${errEmail})`,
-//   icon: 'error',
-//   confirmButtonColor: '#b91c1c',
-// });
-
-// MySwal.fire({
-//   title: `☹️ credential error: ${errorOAuthCredential}`,
-//   icon: 'error',
-//   confirmButtonColor: '#b91c1c',
-// });
-
-// Toast.fire({
-//   icon: 'success',
-//   title: `Hi ${user.displayName} 🫰`,
-//   confirmButtonText: 'hi!',
-// });
-
-// Toast.fire({
-//   icon: 'success',
-//   title: `Welcome back ${user.displayName} 🫰`,
-//   confirmButtonText: 'hi!',
-// });
-
-// MySwal.fire({ title: 'Profile updated!', icon: 'success', confirmButtonColor: '#4ade80' });
-
-// MySwal.fire({
-//   title: 'Something went wrong ☹ please try again',
-//   icon: 'error',
-//   confirmButtonColor: '#b91c1c',
 // });
 
 export default swal;
