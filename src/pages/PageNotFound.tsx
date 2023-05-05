c1c6c5e9ac11c04ea1bea350cc7efe1b76b2706e
tingyusu1786 <tingyusu1786@gmail.com>
2023-05-05 17:51:52 +0800

    update 404 background color

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -4,7 +4,7 @@ function PageNotFound() {
   return (
     <main className='bg-boxes relative min-h-[calc(100vh-64px)] bg-fixed px-10 py-20 text-center'>
       <div className='mb-6 bg-gradient-to-r text-7xl '>
-        <span className='relative inline-block before:absolute before:-inset-1 before:mb-4 before:block before:skew-y-3 before:bg-gradient-to-r before:from-violet-500 before:to-fuchsia-500'>
+        <span className='relative inline-block before:absolute before:-inset-1 before:mb-4 before:block before:skew-y-3 before:bg-gradient-to-br before:from-violet-500 before:to-red-500'>
           <span className='relative text-white'>404</span>
         </span>{' '}
         Page not found ☹
