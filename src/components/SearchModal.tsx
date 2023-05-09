3b97f5cd10d9c75000612fcada11d4a9c2ef3920
tingyusu1786 <tingyusu1786@gmail.com>
2023-05-09 13:13:21 +0800

    put algolia id & key away

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -7,8 +7,7 @@ import algoliasearch from 'algoliasearch/lite';
 import dbApi from '../utils/dbApi';
 import { InstantSearch, SearchBox, Hits, Highlight, Index, useInstantSearch } from 'react-instantsearch-hooks-web';
 
-const searchClient = algoliasearch('CQCQ45KM4I', '343b0909e26f2653041deba6e5b7b442'); //the public API key to use in your frontend code. This key is only usable for search queries and sending data to the Insights API.
-// const searchClient = algoliasearch(process.env.REACT_APP_ALGOLIA_APP_ID!, process.env.REACT_APP_ALGOLIA_SEARCH_KEY!);
+const searchClient = algoliasearch(process.env.REACT_APP_ALGOLIA_APP_ID!, process.env.REACT_APP_ALGOLIA_SEARCH_KEY!);
 
 function BrandHit({ hit }: any) {
   const dispatch = useAppDispatch();
