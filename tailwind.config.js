4b867bef8000d7b84090afe9ee4b489042337d70
tingyusu1786 <tingyusu1786@gmail.com>
2023-05-10 15:54:49 +0800

    remove unused style declaration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,6 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      fontFamily: {
-        heal: ['heal', 'sans-serif'],
-        sayger: ['sayger', 'sans-serif'],
-        tpBold: ['tp-bold', 'sans-serif'],
-        tpReg: ['tp-reg', 'sans-serif'],
-        tpLight: ['tp-light', 'sans-serif'],
-      },
       animation: {
         shrinkSpin: 'shrinkSpin 5s ease-in infinite',
         wave: 'wave 1.5s linear infinite',
@@ -22,11 +15,9 @@ module.exports = {
         shrinkSpin: {
           from: {
             transform: 'rotate(45deg)',
-            // transform: 'scale(1)',
           },
           to: {
             transform: 'rotate(10844deg)',
-            // transform: 'scale(0.1)',
           },
         },
         wave: {
@@ -98,18 +89,10 @@ module.exports = {
     screens: {
       xl2: { max: '1539px' },
       xl: { max: '1279px' },
-      // => @media (max-width: 1279px) { ... }
-
       lg: { max: '1023px' },
-      // => @media (max-width: 1023px) { ... }
-
       md: { max: '767px' },
-      // => @media (max-width: 767px) { ... }
-
       sm: { max: '639px' },
-      // => @media (max-width: 639px) { ... }
       xs: { max: '399px' },
-      // => @media (max-width: 399px) { ... }
     },
   },
   plugins: [],
