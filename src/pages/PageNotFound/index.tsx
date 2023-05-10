ffe4040ba515f0ec5a5dd14ecd31268e3bab3bbb
tingyusu1786 <tingyusu1786@gmail.com>
2023-05-10 16:32:13 +0800

    update html tags

--- src/pages/PageNotFound/index.tsx
+++ src/pages/PageNotFound/index.tsx
@@ -6,15 +6,15 @@ function PageNotFound() {
       <div className='mb-6 bg-gradient-to-r text-7xl '>
         <span className='relative inline-block before:absolute before:-inset-1 before:mb-4 before:block before:skew-y-3 before:bg-gradient-to-br before:from-violet-500 before:to-red-500'>
           <span className='relative text-white'>404</span>
-        </span>{' '}
-        Page not found ☹
+        </span>
+        <span> Page not found ☹</span>
       </div>
       <div className='text-3xl'>
-        Got lost?{' '}
+        <span>Got lost? </span>
         <Link to='/drinkipedia' className='decoration-2 underline-offset-4 hover:underline'>
           Go to drinkipedia
-        </Link>{' '}
-        to find your way
+        </Link>
+        <span> to find your way</span>
       </div>
     </main>
   );
