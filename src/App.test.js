bdb62c9870421b23ab0fee692dfbbddc9f295302
tingyusu1786 <tingyusu1786@gmail.com>
2023-05-13 16:14:32 +0800

    edit file name

---
+++ src/App.test.js
@@ -0,0 +1,28 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import { Provider } from 'react-redux';
+// import { store } from './app/store';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   );
+
+//   expect(getByText(/learn/i)).toBeInTheDocument();
+// });
+
+// function add(a,b){
+//   return a+b
+// }
+
+// describe('add function', () => {
+//   it('should adds 1 + 2 to equal 3', () => {
+//     expect(add(1, 2)).toBe(3);
+//   });
+//   test('adds -1 + -2 to equal -3', () => {
+//     expect(add(-1, -2)).toBe(-3);
+//   });
+// });
