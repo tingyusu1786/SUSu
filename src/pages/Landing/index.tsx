e2e495a8748aa8551fb8f56c18d095fde9eaeee8
tingyusu1786 <tingyusu1786@gmail.com>
2023-05-20 01:11:32 +0800

    update font size to be based on screen size

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -4,30 +4,28 @@ import Typed from 'typed.js';
 
 function Landing() {
   const el = useRef(null);
-  const typed = useRef<any>(null);
+  const typed = useRef<Typed | null>(null);
 
   useEffect(() => {
-    const options = {
-      strings: ['Drink.^600 Log.^600 Repeat.^200 '],
-      stringss: [
-        'npm install^1000\n `installing components...` ^1000\n `Fetching from source...`',
-      ],
-      typeSpeed: 40,
-      backSpeed: 30,
-      loop: true,
-    };
-
-    typed.current = new Typed(el.current, options);
+    if (el.current) {
+      const options = {
+        strings: ['Drink.^600 Log.^600 Repeat.^200 '],
+        typeSpeed: 40,
+        backSpeed: 30,
+        loop: true,
+      };
+      typed.current = new Typed(el.current, options);
+    }
 
     return () => {
-      typed.current.destroy();
+      typed.current && typed.current.destroy();
     };
   }, []);
 
   return (
     <main className='bg-boxes flex min-h-[calc(100vh-64px)] flex-col items-center justify-center bg-fixed px-36'>
-      <div className='type-wrap flex-col items-center justify-start text-center text-9xl selection:bg-green-400'>
-        {<span style={{ whiteSpace: 'pre' }} ref={el} />}
+      <div className='type-wrap flex-col items-center justify-start text-center text-[8vw] selection:bg-green-400'>
+        <span style={{ whiteSpace: 'pre' }} ref={el} />
       </div>
     </main>
   );
